e172b683e39fcaac83a1a4c750d21e43e9802140
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-31 20:22:33 +0100

    Update bash.sh

--- bin/bash.sh
+++ bin/bash.sh
@@ -1,6 +1,12 @@
 dotnet add package Algolia.Search --version 7.0.0
-dotnet new console -n AlgoliaExample
-cd AlgoliaExample
-dotnet add package Algolia.Search --version 7.0.0
+dotnet new console -n seekit
+cd seekit
+dotnet add package seekit.Search --version 7.0.0
 # .dll
-dotnet add package Algolia.Search --version 7.0.0
+dotnet add package.Search --version 7.0.0
+
+curl -O https://download.swift.org/swiftly/darwin/swiftly.pkg && \
+installer -pkg swiftly.pkg -target CurrentUserHomeDirectory && \
+~/.swiftly/bin/swiftly init --quiet-shell-followup && \
+. "${SWIFTLY_HOME_DIR:-$HOME/.swiftly}/env.sh" && \
+hash -r
